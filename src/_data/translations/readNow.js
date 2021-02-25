1bed3826b964195db4bc006d552ce43f2e225ad1
James Doc <hi@jamesdoc.com>
2021-02-25 17:22:52 +0000

    Link to today devotion

---
+++ src/_data/translations/readNow.js
@@ -0,0 +1,8 @@
+module.exports = {
+  es: {
+    readNow: 'Lea la devoción de hoy'
+  },
+  en_gb: {
+    readNow: 'Read today\'s devotion'
+  }
+}
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,3 +10,19 @@ if (!currentPath || currentPath === '/') {
     window.location = `/${lang}/`
   }
 }
+
+const todayBlock = document.querySelector('.js-today');
+if (todayBlock) {
+  const readLink = todayBlock.querySelector('a');
+
+  // Todo: Extract, Tidy and cater for leap year
+  var now = new Date();
+  var start = new Date(now.getFullYear(), 0, 0);
+  var diff = now - start;
+  var oneDay = 1000 * 60 * 60 * 24;
+  var day = Math.floor(diff / oneDay);
+
+  const url = `/${todayBlock.dataset.locale}/${todayBlock.dataset.variant}/${day}`;
+
+  readLink.href = url;
+}
