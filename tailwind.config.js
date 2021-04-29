508ede18ab0a6d10c04006ffaaae171c60718c38
James Doc <hi@jamesdoc.com>
2021-04-29 11:09:29 +0100

    Add in Work Sans

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'sans': ['"Work Sans"', 'Helvetica', 'Arial', 'sans-serif'],
+    },
     extend: {},
   },
   variants: {
