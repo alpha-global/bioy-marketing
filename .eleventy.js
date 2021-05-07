31d8f2096119343f121be210e525293cd428a5ea
Jannie Theunissen <jannie@onesheep.org>
2021-05-07 16:43:49 +0200

    paurakh's markdown filter working again

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,6 @@
-module.exports = function(eleventyConfig) {
+const markdownIt = require("markdown-it");
+
+module.exports = function (eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
 
   eleventyConfig.addPassthroughCopy('src/assets/icon');
@@ -11,11 +13,9 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addLayoutAlias("page", "layouts/page.njk");
   eleventyConfig.addLayoutAlias("commentary", "layouts/commentary.njk");
 
-  const MarkdownIt = require("markdown-it");
-  const mdRender = new MarkdownIt();
 
-  // this thing breaks EVERYTHING:
-  eleventyConfig.addFilter("markdown", function (rawString) {
+  eleventyConfig.addFilter("markdown", (rawString) => {
+    const mdRender = new markdownIt({ html: true });
     const rendered = mdRender.render(rawString);
     // return eleventyConfig.getFilter("safe")(rendered);  // not sure why this is not working for us
     return rendered;
