73c7158d55cd79541e26887d2814dad0560f5a7e
Jannie Theunissen <jannie@onesheep.org>
2021-05-10 17:56:35 +0200

    the call to action

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [
     'src/**/*.njk',
@@ -9,6 +11,22 @@ module.exports = {
       'sans': ['"AvantGardeGothicITC"', 'Helvetica', 'Arial', 'sans-serif'],
       'commentary': ['"Work Sans"', 'Helvetica', 'Arial', 'sans-serif'],
       'scripture': ['"Merriweather"', 'serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      grey: {
+        DEFAULT: '#6A6A6A',
+      },
+      stone: {
+        DEFAULT: '#F6F2F1',
+      },
+      red: {
+        DEFAULT: '#E42312',
+        dark: '#cf2022'
+      }
     },
     extend: {},
   },
--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -2,7 +2,7 @@ module.exports = {
 
   // Usage: {% svgSprite name="icon" %}
   svgSprite:  (svgSprite) => {
-    return `<svg class="icon icon--${svgSprite.name}" role="img" aria-hidden="true">
+    return `<svg class="w-full" role="img" aria-hidden="true">
               <use xlink:href="/assets/svg/sprite.svg#${svgSprite.name}"></use>
             </svg>`;
   }
