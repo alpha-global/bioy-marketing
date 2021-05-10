baebe96254c0901c6cb66a66aa89c0cafc30ce1e
James Doc <hi@jamesdoc.com>
2021-05-10 20:39:00 +0100

    Tighten up

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -2,7 +2,7 @@ module.exports = {
 
   // Usage: {% svgSprite name="icon" %}
   svgSprite:  (svgSprite) => {
-    return `<svg class="w-full" role="img" aria-hidden="true">
+    return `<svg class="${svgSprite.class}" role="img" aria-hidden="true">
               <use xlink:href="/assets/svg/sprite.svg#${svgSprite.name}"></use>
             </svg>`;
   }
