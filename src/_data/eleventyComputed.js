3a1777c72f1042650f3ffc7214be061292f5e2b0
Paurakh Sharma Humagain <paurakh011@gmail.com>
2021-05-10 10:40:43 +0545

    Fix eleventyComputed for help items

--- src/_data/eleventyComputed.js
+++ src/_data/eleventyComputed.js
@@ -2,5 +2,6 @@
 module.exports = {
   label: data => data.labels[data.locale],
   site: data => data.siteLocal[data.locale],
-  lang: data => data[data.locale]
+  lang: data => data[data.locale],
+  blocks: data => data.blocks[data.locale]
 };
