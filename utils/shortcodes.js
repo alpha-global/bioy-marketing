3d6891960a2e39f657b482f66b073514631f9303
Jannie Theunissen <jannie@onesheep.org>
2021-05-11 12:46:35 +0200

    sans image and references

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -1,9 +1,39 @@
+const Image = require("@11ty/eleventy-img");
+
 module.exports = {
 
   // Usage: {% svgSprite name="icon" %}
-  svgSprite:  (svgSprite) => {
+  svgSprite: (svgSprite) => {
     return `<svg class="${svgSprite.class}" role="img" aria-hidden="true">
               <use xlink:href="/assets/svg/sprite.svg#${svgSprite.name}"></use>
             </svg>`;
+  },
+
+  blockImage: (src, alt, sizes = "100vw", classes = "w-full") => {
+    const options = {
+      widths: [320, 640, 800, 1200],
+      formats: ['webp', 'jpeg'],
+      urlPath: ".src/assets/img/",
+      outputDir: "./public/assets/img"
+    };
+
+    const imageAttributes = {
+      class: classes,
+      alt: alt,
+      sizes: sizes,
+      loading: "lazy",
+      decoding: "async"
+    };
+
+
+    return imageGen(src, options, imageAttributes);
   }
+
+
+}
+
+function imageGen(src, options, attributes) {
+  Image(src, options);
+  metadata = Image.statsSync(src, options);
+  return Image.generateHTML(metadata, attributes);
 }
