37121a21fce955f8c1fcbe4340a27e8a71af45f3
Paurakh Sharma Humagain <paurakh011@gmail.com>
2021-05-11 13:20:46 +0545

    Make devotion secreen better looking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,50 @@
-const colors = require('tailwindcss/colors')
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: [
-    'src/**/*.njk',
-    'src/**/*.html',
-  ],
+  purge: ["src/**/*.njk", "src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
-      'sans': ['"AvantGardeGothicITC"', 'Helvetica', 'Arial', 'sans-serif'],
-      'commentary': ['"Work Sans"', 'Helvetica', 'Arial', 'sans-serif'],
-      'scripture': ['"Merriweather"', 'serif'],
+      sans: ['"AvantGardeGothicITC"', "Helvetica", "Arial", "sans-serif"],
+      commentary: ['"Work Sans"', "Helvetica", "Arial", "sans-serif"],
+      scripture: ['"Merriweather"', "serif"],
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: colors.black,
       white: colors.white,
       grey: {
-        DEFAULT: '#6A6A6A',
+        DEFAULT: "#6A6A6A",
       },
       stone: {
-        DEFAULT: '#F6F2F1',
+        DEFAULT: "#F6F2F1",
       },
       red: {
-        DEFAULT: '#E42312',
-        dark: '#cf2022'
-      }
+        DEFAULT: "#E42312",
+        dark: "#cf2022",
+      },
     },
-    extend: {},
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            h3: {
+              color: theme('colors.red.DEFAULT'),
+              marginTop: '1em',
+              marginBottom: 0,
+            },
+            h2: {
+              marginTop: 0,
+              marginBottom: 0,
+            }
+          },
+        },
+      }),
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -5,5 +5,28 @@ module.exports = {
     return `<svg class="${svgSprite.class}" role="img" aria-hidden="true">
               <use xlink:href="/assets/svg/sprite.svg#${svgSprite.name}"></use>
             </svg>`;
+  },
+  devotionSection: (devotionSection) => {
+    return `
+    <section>
+    <div class="p-4 bg-red">
+      <div class="font-extrabold text-4xl text-white">${devotionSection.title}</div>
+    </div>
+    <div class="p-4">
+      <h2>${devotionSection.reference}</h1>
+        <div class="font-scripture">
+          ${devotionSection.text}
+        </div>
+        <h2>Commentary</h2>
+        ${devotionSection.comment}
+
+
+        <h2>Prayer</h2>
+        <div class="border-l-4 pl-4 border-red font-scripture">
+          ${devotionSection.prayer}
+        </div>
+    </div>
+  </section>
+    `
   }
 }
