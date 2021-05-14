b1c072ba88201f6a633180ff09d644b686591de6
Jannie Theunissen <jannie@onesheep.org>
2021-05-14 15:38:08 +0200

    media channels moved to blocks

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -12,6 +12,7 @@ const app = new function () {
       
     init() {
       window.onpopstate = redirectBaseUrl();
+      setMailPopup();
     
       const testimonySwiper = new Swipe(document.getElementById('slider'), {
         startSlide: 0,
@@ -38,6 +39,25 @@ const app = new function () {
     }
   }
 
+  function setMailPopup() {
+
+    const mailerLiteLink = document.querySelector('.js-mailerLite');
+    if (!mailerLiteLink) return;
+    (function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
+    var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
+    f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
+    var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
+    _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
+
+    var ml_account = ml('accounts', '3056200', 'f0i3r5m4t5', 'load');
+
+    mailerLiteLink.addEventListener('click', function(e) {
+      e.preventDefault();
+      let d = mailerLiteLink.dataset.ml.split(',');
+      ml_account('webforms', String(d[0]), String(d[1]), 'show');
+    });
+  }
+
 }
 
 
@@ -48,22 +68,6 @@ if (document.readyState != 'loading') {
 }
 
 
-const mailerLiteLink = document.querySelector('.js-mailerLite');
-if (mailerLiteLink) {
-  (function(m,a,i,l,e,r){ m['MailerLiteObject']=e;function f(){
-    var c={ a:arguments,q:[]};var r=this.push(c);return "number"!=typeof r?r:f.bind(c.q);}
-    f.q=f.q||[];m[e]=m[e]||f.bind(f.q);m[e].q=m[e].q||f.q;r=a.createElement(i);
-    var _=a.getElementsByTagName(i)[0];r.async=1;r.src=l+'?v'+(~~(new Date().getTime()/1000000));
-    _.parentNode.insertBefore(r,_);})(window, document, 'script', 'https://static.mailerlite.com/js/universal.js', 'ml');
-
-  var ml_account = ml('accounts', '3056200', 'f0i3r5m4t5', 'load');
-
-  mailerLiteLink.addEventListener('click', function(e) {
-    e.preventDefault();
-    let d = mailerLiteLink.dataset.ml.split(',');
-    ml_account('webforms', String(d[0]), String(d[1]), 'show');
-  });
-}
 
 window.store = function() {
   return {
