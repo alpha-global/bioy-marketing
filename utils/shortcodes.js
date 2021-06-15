489105bde57aff1d1ff61c354114d25e3f7ed7dd
James Doc <hi@jamesdoc.com>
2021-06-15 15:55:28 +0100

    Cache buste the SVG sprite

--- utils/shortcodes.js
+++ utils/shortcodes.js
@@ -1,10 +1,11 @@
 const Image = require("@11ty/eleventy-img");
+const { cacheBuster } = require('../src/_data/globals');
 
 module.exports = {
   // Usage: {% svgSprite name="icon" %}
   svgSprite: (svgSprite) => {
     return `<svg class="${svgSprite.class}" role="img" aria-hidden="true">
-              <use xlink:href="/assets/svg/sprite.svg#${svgSprite.name}"></use>
+              <use xlink:href="/assets/svg/sprite.svg?${cacheBuster}#${svgSprite.name}"></use>
             </svg>`;
   },
 
