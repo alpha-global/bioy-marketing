34ed4072e09baeb75a9eaf9e23319fdff2534b3e
Timothy Koech <timosville@gmail.com>
2021-10-18 10:40:20 +0300

    refactor: rename collections

--- utils/collections.js
+++ utils/collections.js
@@ -1,21 +1,21 @@
 const slugify = require('slugify')
-const help = require("../src/help/help.11tydata.json");
+const faqs = require("../src/faqs/faqs.11tydata.json");
 
-exports.helpItems = collection => {
+exports.faqsItems = collection => {
   items = [];
 
-    Object.keys(help).forEach(locale => {
-        help[locale].helpItems.forEach(item => {
-          item.locale = locale;
-          item.slug = slugify(item.title,{
-            lower: true,
-            remove: /[*+~.,<>()?&%$@'"!:@]/g,
-            replacement: '-',
-            locale: locale
-          });
-          items.push(item);
-        });
+  Object.keys(faqs).forEach(locale => {
+    faqs[locale].faqsItems.forEach(item => {
+      item.locale = locale;
+      item.slug = slugify(item.title, {
+        lower: true,
+        remove: /[*+~.,<>()?&%$@'"!:@]/g,
+        replacement: '-',
+        locale: locale
+      });
+      items.push(item);
     });
+  });
 
-    return items;
+  return items;
 }
