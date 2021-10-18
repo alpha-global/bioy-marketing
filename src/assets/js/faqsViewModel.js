78ec028a47441880c65254c7918668d9e9945966
Timothy Koech <timosville@gmail.com>
2021-10-18 14:57:42 +0300

    refactor: change names function name

---
+++ src/assets/js/faqsViewModel.js
@@ -0,0 +1,33 @@
+export default {
+  query: null,
+  show: false,
+  faqsItems: [],
+
+  init() {
+    this.faqsItems = document.querySelectorAll('.js-faqsItem');
+
+    const filterContainer = document.querySelector('.js-filter');
+    filterContainer.classList.remove('hidden');
+  },
+
+  filteredItems() {
+    if (!this.query) {
+      this.faqsItems.forEach(item => {
+        item.classList.remove('hidden');
+      });
+      return null;
+    }
+
+    let results = 0;
+
+    this.faqsItems.forEach(item => {
+      if (item.innerText.includes(this.query)) {
+        item.classList.remove('hidden');
+        results++;
+      } else {
+        item.classList.add('hidden');
+      }
+    });
+    return results;
+  }
+}
