3a65c1278b779d5c1179c51c8aacc00d8dbb30c2
Timothy Koech <timosville@gmail.com>
2021-11-09 15:12:36 +0300

    fix: slugify failing due to empty title

--- utils/collections.js
+++ utils/collections.js
@@ -6,6 +6,7 @@ exports.faqsItems = collection => {
 
   Object.keys(faqs).forEach(locale => {
     faqs[locale].faqsItems.forEach(item => {
+      if (!item.title) return;
       item.locale = locale;
       item.slug = slugify(item.title, {
         lower: true,
