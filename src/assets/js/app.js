b3920ed3d96681a2c811ac9f8f81f3194f2340a2
Timothy Koech <timosville@gmail.com>
2021-11-09 16:58:16 +0300

    style: add hover classes

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,6 +8,8 @@ const { locales } = require("../../_data/globals");
 
 import helpers from "./helpers.js";
 
+import podcastSwiper from "./podcastSwiper.js";
+
 window.app = new (function () {
   // public functions
   return {
@@ -23,50 +25,13 @@ window.app = new (function () {
         disableScroll: false,
         stopPropagation: true,
       });
-
-      const podcastSwiper = new Swipe(document.getElementById("podcast"), {
-        callback: function (index, element) {},
-        transitionEnd: function (index, element) {},
-      });
-
-      const previousPodcast = document.getElementById("previous");
-      const nextPodcast = document.getElementById("next");
-
-      const firstPodcastPagination = document.getElementById(
-        "firstPodcastPagination"
-      );
-      const secondPodcastPagination = document.getElementById(
-        "secondPodcastPagination"
-      );
-      const thirdPodcastPagination = document.getElementById(
-        "thirdPodcastPagination"
-      );
-
-      nextPodcast.addEventListener("click", function () {
-        podcastSwiper.next();
-      });
-
-      previousPodcast.addEventListener("click", function () {
-        podcastSwiper.prev();
-      });
-
-      firstPodcastPagination.addEventListener("click", function () {
-        podcastSwiper.slide(0, 100);
-      });
-
-      secondPodcastPagination.addEventListener("click", function () {
-        podcastSwiper.slide(1, 100);
-      });
-
-      thirdPodcastPagination.addEventListener("click", function () {
-        podcastSwiper.slide(2, 100);
-      });
     },
 
     baseViewModel,
     faqsViewModel,
     mediumViewModel,
     helpers,
+    podcastSwiper,
   };
 
   function setMailPopup() {
---
+++ src/assets/js/podcastSwiper.js
@@ -0,0 +1,43 @@
+import Swipe from "swipejs";
+
+export default {
+  init() {
+    const podcastSwiper = new Swipe(document.getElementById("podcast"), {
+      callback: function (index, element) {},
+      transitionEnd: function (index, element) {},
+    });
+
+    const previousPodcast = document.getElementById("previous");
+    const nextPodcast = document.getElementById("next");
+
+    const firstPodcastPagination = document.getElementById(
+      "firstPodcastPagination"
+    );
+    const secondPodcastPagination = document.getElementById(
+      "secondPodcastPagination"
+    );
+    const thirdPodcastPagination = document.getElementById(
+      "thirdPodcastPagination"
+    );
+
+    nextPodcast.addEventListener("click", function () {
+      podcastSwiper.next();
+    });
+
+    previousPodcast.addEventListener("click", function () {
+      podcastSwiper.prev();
+    });
+
+    firstPodcastPagination.addEventListener("click", function () {
+      podcastSwiper.slide(0, 100);
+    });
+
+    secondPodcastPagination.addEventListener("click", function () {
+      podcastSwiper.slide(1, 100);
+    });
+
+    thirdPodcastPagination.addEventListener("click", function () {
+      podcastSwiper.slide(2, 100);
+    });
+  },
+};
