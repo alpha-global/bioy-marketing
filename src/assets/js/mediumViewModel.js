9d4659a078402695e8f4aefe0ba3ab4ea8f0a7fb
Timothy Koech <timosville@gmail.com>
2021-11-11 14:21:06 +0300

    refactor: use transitionEnd to change active class

--- src/assets/js/mediumViewModel.js
+++ src/assets/js/mediumViewModel.js
@@ -2,7 +2,6 @@ import Swipe from "swipejs";
 
 export default {
   mediaFocus: 0,
-  currentElementIndex: 0,
 
   mediaClass(node) {
     return {
@@ -18,22 +17,30 @@ export default {
       this.$refs.mediaNav.classList.remove("hidden");
       this.$refs.mediaContainer.classList.remove("flex-col");
     }
+    const previousPodcast = document.getElementById("previous");
+    const nextPodcast = document.getElementById("next");
+    const paginationButtons = document.querySelectorAll(".podcast-pagination");
+
     const podcastSwiper = new Swipe(document.getElementById("podcast"), {
       startSlide: 0,
-      speed: 500,
+      speed: 200,
       auto: 6000,
       draggable: false,
       continuous: true,
       disableScroll: true,
-      callback: function (index, element) {},
       transitionEnd: function (index, element) {
-        this.currentElementIndex = element.getAttribute("data-index");
-        console.log(this.currentElementIndex);
+        const currentElementIndex = element.getAttribute("data-index");
+        paginationButtons.forEach((button) =>
+          button.getAttribute("data-index") === currentElementIndex
+            ? button.classList.add("bg-white")
+            : button.classList.remove("bg-white")
+        );
       },
     });
 
-    const previousPodcast = document.getElementById("previous");
-    const nextPodcast = document.getElementById("next");
+    podcastSwiper.getPos() === 0
+      ? paginationButtons[0].classList.add("bg-white")
+      : null;
 
     nextPodcast.addEventListener("click", function () {
       podcastSwiper.next();
@@ -43,7 +50,6 @@ export default {
       podcastSwiper.prev();
     });
 
-    const paginationButtons = document.querySelectorAll(".podcast-pagination");
     paginationButtons.forEach((button) => {
       button.addEventListener("click", () =>
         podcastSwiper.slide(button.getAttribute("data-index"), 200)
