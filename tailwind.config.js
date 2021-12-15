935a8a21a611455d2bb9ff0856aeb0927c06e096
Timothy Koech <timosville@gmail.com>
2021-12-15 08:24:50 +0300

    Migrate to tailwind css 30 (#42)
    * feat: restore youth data file
    
    * feat: display button if edition exists
    
    * feat: center buttons
    
    * refactor: show podcasts if channel exists
    
    * feat: show buttons for available channnels
    
    * Update Repeated Content Blocks “media”
    
    * feat: style buttons
    
    * fix: javascript query errors
    
    * feat: minor changes
    
    * feat: update data file
    
    * feat: restore rounded corners
    
    * feat: localized urls
    
    * feat: add translatablenavigation fields
    
    * feat: localize navigation bar items
    
    * feat: make line breaks work
    
    * feat: localize url
    
    * feat: add cookie translations
    
    * feat: add id
    
    * feat: add translations for faqs navigation button
    
    * feat: upgrade tailwind
    
    * refactor: purging css
    
    * feat: remove variants
    
    Co-authored-by: OneSheep <admin@onesheep.org>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,5 @@
 module.exports = {
-  purge: {
-    content: ["src/**/*.njk", "src/**/*.html", "src/**/*.js"],
-    options: {
-      safelist: ['bg-grey-light', 'bg-transparent'],
-    }
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: ["./src/**/*.{js,html,njk}"],
   theme: {
     fontFamily: {
       sans: ['"AvantGardeGothicITC"', "Helvetica", "Arial", "sans-serif"],
@@ -13,25 +7,26 @@ module.exports = {
       scripture: ['"Merriweather"', "serif"],
     },
     boxShadow: {
-      none: '0 0 #000',
-      sm: '0 -1px 2px 0 rgba(0, 0, 0, 0.05)',
-      DEFAULT: '0 -1px 3px 0 rgba(0, 0, 0, 0.1), 0 -1px 2px 0 rgba(0, 0, 0, 0.06)',
-      md: '0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)',
-      lg: '0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 -4px 6px -2px rgba(0, 0, 0, 0.05)',
-      xl: '0 -20px 25px -5px rgba(0, 0, 0, 0.1), 0 -10px 10px -5px rgba(0, 0, 0, 0.04)',
-      '2xl': '0 -25px 50px -12px rgba(0, 0, 0, 0.25)',
+      none: "0 0 #000",
+      sm: "0 -1px 2px 0 rgba(0, 0, 0, 0.05)",
+      DEFAULT:
+        "0 -1px 3px 0 rgba(0, 0, 0, 0.1), 0 -1px 2px 0 rgba(0, 0, 0, 0.06)",
+      md: "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 -2px 4px -1px rgba(0, 0, 0, 0.06)",
+      lg: "0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 -4px 6px -2px rgba(0, 0, 0, 0.05)",
+      xl: "0 -20px 25px -5px rgba(0, 0, 0, 0.1), 0 -10px 10px -5px rgba(0, 0, 0, 0.04)",
+      "2xl": "0 -25px 50px -12px rgba(0, 0, 0, 0.25)",
     },
     colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
+      transparent: "transparent",
+      current: "currentColor",
       black: {
-        DEFAULT: '#333333',
-        scrim: '#00000075',
+        DEFAULT: "#333333",
+        scrim: "#00000075",
       },
-      white: '#ffffff',
+      white: "#ffffff",
       grey: {
-        light: '#EFEFEF',
-        DEFAULT: '#6A6A6A',
+        light: "#EFEFEF",
+        DEFAULT: "#6A6A6A",
       },
       stone: {
         DEFAULT: "#F6F2F1",
@@ -46,52 +41,37 @@ module.exports = {
         DEFAULT: {
           css: {
             h3: {
-              color: theme('colors.red.DEFAULT'),
-              marginTop: '1em',
+              color: theme("colors.red.DEFAULT"),
+              marginTop: "1em",
               marginBottom: 0,
             },
             h2: {
               marginTop: 0,
               marginBottom: 0,
-            }
+            },
           },
         },
       }),
     },
     extend: {
       fontSize: {
-        '6xl': ['3.75rem', '4rem'],
-        'title': ['32px', '46px'],
-        'title-medium': ['48px', '67px'],
-        'title-large': ['56px', '78px'],
-        'subtitle': ['20px', '30px'],
-        'subtitle-medium': ['24px', '36px'],
-        'subtitle-large': ['32px', '48px'],
-        'small': ['16px', '24px']
+        "6xl": ["3.75rem", "4rem"],
+        title: ["32px", "46px"],
+        "title-medium": ["48px", "67px"],
+        "title-large": ["56px", "78px"],
+        subtitle: ["20px", "30px"],
+        "subtitle-medium": ["24px", "36px"],
+        "subtitle-large": ["32px", "48px"],
+        small: ["16px", "24px"],
       },
       scale: {
-        '25': '.25',
-        '-1': '-1',
+        25: ".25",
+        "-1": "-1",
       },
       screens: {
-        'xs': '512px',
+        xs: "512px",
       },
     },
   },
-  variants: {
-    extend: {
-      borderRadius: ['direction'],
-      display: ['direction'],
-      inset: ['direction'],
-      flexDirection: ['direction'],
-      scale: ['direction'],
-      textAlign: ['direction'],
-      transform: ['direction'],
-      padding: ['direction'],
-    },
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require('tailwindcss-dir')(),
-  ],
+  plugins: [require("@tailwindcss/typography"), require("tailwindcss-dir")()],
 };
