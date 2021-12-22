e42457bbba93284e7c14e056e853fc620af8b04a
Timothy Koech <timosville@gmail.com>
2021-12-22 19:41:38 +0300

    Enable new languages on bibleinoneyearorg (#51)
    * feat: fetch devotion from 24th day for German locale
    
    * feat: add all devotions for German locale
    
    * refactor: order languages alphabetically
    
    * feat: add other languages

--- src/_data/bioy.js
+++ src/_data/bioy.js
@@ -1,6 +1,4 @@
-
 const Cache = require("@11ty/eleventy-cache-assets");
-
 
 /**
  * Fetch devotion range
@@ -10,19 +8,34 @@ const Cache = require("@11ty/eleventy-cache-assets");
  * @param {String} variant
  * @returns {Promise<Array>}
  */
-async function fetchDevotionRange(from = 1, to = 365, locale = 'en_GB', variant = 'classic') {
+async function fetchDevotionRange(
+  from = 1,
+  to = 365,
+  locale = "en_GB",
+  variant = "classic"
+) {
+  if (
+    locale === "de" ||
+    locale === "th" ||
+    locale === "fr" ||
+    locale === "id"
+  ) {
+    from = 1;
+  }
   const url = `https://api.bioydata.com/api/v2/devotion/from/${from}/to/${to}?locale=${locale}&variant=${variant}&format=html`;
   let devotions = await _fetchDevotionRangeFromUrl(url, []);
   devotions.forEach((_, index) => {
     // To build the pages and cool URIs later we'll need lto keep hold of
     // the locale and variant…
-    const lang = locale.split('_')[0];
-    devotions[index]['locale'] = lang;
-    devotions[index]['variant'] = variant;
-    devotions[index]['url'] = `/${lang}/${variant}/${devotions[index]['devotionId']}`;
-  })
+    const lang = locale.split("_")[0];
+    devotions[index]["locale"] = lang;
+    devotions[index]["variant"] = variant;
+    devotions[index][
+      "url"
+    ] = `/${lang}/${variant}/${devotions[index]["devotionId"]}`;
+  });
 
-  return devotions
+  return devotions;
 }
 
 /**
@@ -34,20 +47,19 @@ async function fetchDevotionRange(from = 1, to = 365, locale = 'en_GB', variant 
 async function _fetchDevotionRangeFromUrl(url, devotions) {
   let json = await Cache(url, {
     duration: "1d",
-    type: "json"
+    type: "json",
   });
 
-  devotions.push(...json['data'])
+  devotions.push(...json["data"]);
 
-  if (json['links']['next'] != null) {
-    await _fetchDevotionRangeFromUrl(json['links']['next'], devotions)
+  if (json["links"]["next"] != null) {
+    await _fetchDevotionRangeFromUrl(json["links"]["next"], devotions);
   }
 
-  return devotions
+  return devotions;
 }
 
 module.exports = async function (fullImport = false) {
-
   let startDayNumber = 1;
   let endDayNumber = 365;
 
@@ -62,24 +74,28 @@ module.exports = async function (fullImport = false) {
   }
 
   try {
-
     const variants = {
-      'classic': ['en_Gb', 'es', 'ar', 'hi', 'zh_Hans', 'fr', 'de', 'th'],
-      'youth': ['en_Gb'],
-      'express': ['en_Gb', 'de']
-    }
+      classic: ["en_Gb", "es", "ar", "hi", "zh_Hans", "fr", "de", "th", "id"],
+      youth: ["en_Gb", "ar"],
+      express: ["en_Gb", "de", "ar"],
+    };
     const data = [];
 
     for (const [variant, locales] of Object.entries(variants)) {
       for (const locale of locales) {
-        data.push(...await fetchDevotionRange(startDayNumber, endDayNumber, locale, variant))
+        data.push(
+          ...(await fetchDevotionRange(
+            startDayNumber,
+            endDayNumber,
+            locale,
+            variant
+          ))
+        );
       }
     }
 
     return data;
-
   } catch (e) {
     console.log("\n\nSquark; talking to BiOY API failed\n\n");
   }
 };
-
