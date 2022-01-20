cbb6664cb0a7d3196bf2eb312a66096832fd1289
Timothy Koech <timosville@gmail.com>
2022-01-20 16:43:24 +0300

    Website dependancies (#53)
    * Update Repeated Content Blocks “media”
    
    * Update Core Pages “FAQs”
    
    * feat: update laravel-mix
    
    * feat: update tailwindcss
    
    * feat: update tailwindcss dependencies
    
    * refactor: restructure dependencies
    
    * feat: upgrade alpine js
    
    * refactor: align code with Alpine.js v3
    
    * feat: upgrade 11ty image plugin
    
    * chore: better scripts
    
    * fix: copyright symbol not displayed correctly
    
    * refactor: downgrade 11ty image plugin to improve build time
    
    Co-authored-by: OneSheep <admin@onesheep.org>
    Co-authored-by: Yannick Emery <yannick@stbk.org.uk>

--- src/assets/js/baseViewModel.js
+++ src/assets/js/baseViewModel.js
@@ -1,30 +1,32 @@
-export default {
+export default () => {
+  return {
     isCookieBoxOpen: true,
     isLangPickerOpen: false,
 
     acceptCookies() {
-        localStorage.setItem('cookieConsent', "accepted");
-        this.isCookieBoxOpen = false;
-        console.log("box is open: ", this.isCookieBoxOpen);
+      localStorage.setItem("cookieConsent", "accepted");
+      this.isCookieBoxOpen = false;
+      console.log("box is open: ", this.isCookieBoxOpen);
     },
 
     deviceUrl(locale, androidUrl, iosUrl) {
-        const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+      const userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-        if (/android/i.test(userAgent)) {
-            return androidUrl;
-        }
+      if (/android/i.test(userAgent)) {
+        return androidUrl;
+      }
 
-        if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-            return iosUrl;
-        }
-                
-        return `/${locale}/#start`;
+      if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return iosUrl;
+      }
+
+      return `/${locale}/#start`;
     },
 
     init() {
-        const stored = localStorage.getItem('cookieConsent');
-        if ( stored !== "accepted") return;
-        this.isCookieBoxOpen = false;
-    }
-}
+      const stored = localStorage.getItem("cookieConsent");
+      if (stored !== "accepted") return;
+      this.isCookieBoxOpen = false;
+    },
+  };
+};
