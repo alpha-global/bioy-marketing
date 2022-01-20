f68c201f091395d889553822185902e1a8dce2a9
Timothy Koech <timosville@gmail.com>
2022-01-20 14:11:32 +0300

    refactor: align code with Alpine.js v3

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,4 @@
-import "alpinejs";
+import Alpine from "alpinejs";
 import Swipe from "swipejs";
 
 import baseViewModel from "./baseViewModel.js";
@@ -9,67 +9,64 @@ const { locales } = require("../../_data/globals");
 import helpers from "./helpers.js";
 
 window.app = new (function () {
-  // public functions
-  return {
-    init() {
-      setMailPopup();
 
-      const testimonySwiper = new Swipe(document.getElementById("slider"), {
-        startSlide: 0,
-        speed: 500,
-        auto: 6000,
-        draggable: false,
-        continuous: true,
-        disableScroll: false,
-        stopPropagation: true,
-      });
-     
-    },
+  return {
+    init() {},
 
     baseViewModel,
     faqsViewModel,
     mediumViewModel,
     helpers,
   };
-
-  function setMailPopup() {
-    const mailerLiteLink = document.querySelector(".js-mailerLite");
-    if (!mailerLiteLink) return;
-    (function (m, a, i, l, e, r) {
-      m["MailerLiteObject"] = e;
-      function f() {
-        var c = { a: arguments, q: [] };
-        var r = this.push(c);
-        return "number" != typeof r ? r : f.bind(c.q);
-      }
-      f.q = f.q || [];
-      m[e] = m[e] || f.bind(f.q);
-      m[e].q = m[e].q || f.q;
-      r = a.createElement(i);
-      var _ = a.getElementsByTagName(i)[0];
-      r.async = 1;
-      r.src = l + "?v" + ~~(new Date().getTime() / 1000000);
-      _.parentNode.insertBefore(r, _);
-    })(
-      window,
-      document,
-      "script",
-      "https://static.mailerlite.com/js/universal.js",
-      "ml"
-    );
-
-    var ml_account = ml("accounts", "3056200", "f0i3r5m4t5", "load");
-
-    mailerLiteLink.addEventListener("click", function (e) {
-      e.preventDefault();
-      let d = mailerLiteLink.dataset.ml.split(",");
-      ml_account("webforms", String(d[0]), String(d[1]), "show");
-    });
-  }
 })();
 
-if (document.readyState != "loading") {
-  window.app.init();
-} else {
-  document.addEventListener("DOMContentLoaded", window.app.init);
+function setMailPopup() {
+  const mailerLiteLink = document.querySelector(".js-mailerLite");
+  if (!mailerLiteLink) return;
+  (function (m, a, i, l, e, r) {
+    m["MailerLiteObject"] = e;
+    function f() {
+      var c = { a: arguments, q: [] };
+      var r = this.push(c);
+      return "number" != typeof r ? r : f.bind(c.q);
+    }
+    f.q = f.q || [];
+    m[e] = m[e] || f.bind(f.q);
+    m[e].q = m[e].q || f.q;
+    r = a.createElement(i);
+    var _ = a.getElementsByTagName(i)[0];
+    r.async = 1;
+    r.src = l + "?v" + ~~(new Date().getTime() / 1000000);
+    _.parentNode.insertBefore(r, _);
+  })(
+    window,
+    document,
+    "script",
+    "https://static.mailerlite.com/js/universal.js",
+    "ml"
+  );
+
+  var ml_account = ml("accounts", "3056200", "f0i3r5m4t5", "load");
+
+  mailerLiteLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    let d = mailerLiteLink.dataset.ml.split(",");
+    ml_account("webforms", String(d[0]), String(d[1]), "show");
+  });
 }
+
+const testimonySwiper = new Swipe(document.getElementById("slider"), {
+  startSlide: 0,
+  speed: 500,
+  auto: 6000,
+  draggable: false,
+  continuous: true,
+  disableScroll: false,
+  stopPropagation: true,
+});
+
+window.testimonySwiper = testimonySwiper;
+
+setMailPopup();
+
+Alpine.start();
--- src/assets/js/helpers.js
+++ src/assets/js/helpers.js
@@ -1,63 +1,65 @@
-export default {
-  openEdition: false,
-  openLanguages: false,
-  isDevotion: false,
-  outOfView: false,
-  init(variant) {
-    this.switchBanner();
-    this.isDevotion = variant ? true : false;
-  },
+export default () => {
+  return {
+    openEdition: false,
+    openLanguages: false,
+    isDevotion: false,
+    outOfView: false,
+    init(variant) {
+      this.switchBanner();
+      this.isDevotion = variant ? true : false;
+    },
 
-  parsePageUrl(url, locale) {
-    return `/${locale}${url.substring(3)}`;
-  },
+    parsePageUrl(url, locale) {
+      return `/${locale}${url.substring(3)}`;
+    },
 
-  switchBanner() {
-    const headerElement = document.getElementById("header");
+    switchBanner() {
+      const headerElement = document.getElementById("header");
 
-    const switchBannerElement = document.getElementById("switchBanner");
+      const switchBannerElement = document.getElementById("switchBanner");
 
-    const bannerClasses = [
-      "top-0",
-      "inset-x-0",
-      "justify-center",
-      "bg-white",
-      "max-w-2xl",
-      "mx-auto",
-      "divide-y-1",
-      "border-b-2",
-      "border-red",
-      "divide-x-2",
-      "rtl:divide-x-reverse",
-      "divide-grey-light",
-    ];
+      const bannerClasses = [
+        "top-0",
+        "inset-x-0",
+        "justify-center",
+        "bg-white",
+        "max-w-2xl",
+        "mx-auto",
+        "divide-y-1",
+        "border-b-2",
+        "border-red",
+        "divide-x-2",
+        "rtl:divide-x-reverse",
+        "divide-grey-light",
+      ];
 
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach((entry) => {
-        if (this.isDevotion) {
-          if (entry.intersectionRatio > 0) {
-            this.outOfView = false;
-            switchBannerElement.classList.replace("fixed", "absolute");
-            switchBannerElement.classList.remove(...bannerClasses);
-            switchBannerElement.classList.add(
-              "ltr:left-16",
-              "rtl:right-16",
-              "top-10"
-            );
-          } else {
-            this.outOfView = true;
-            switchBannerElement.classList.replace("absolute", "fixed");
-            switchBannerElement.classList.add(...bannerClasses);
-            switchBannerElement.classList.remove(
-              "ltr:left-16",
-              "rtl:right-16",
-              "top-10"
-            );
+      const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (this.isDevotion) {
+            if (entry.intersectionRatio > 0) {
+              this.outOfView = false;
+              switchBannerElement.classList.replace("fixed", "absolute");
+              switchBannerElement.classList.remove(...bannerClasses);
+              switchBannerElement.classList.add(
+                "ltr:left-16",
+                "rtl:right-16",
+                "top-10"
+              );
+            } else {
+              this.outOfView = true;
+              switchBannerElement.classList.replace("absolute", "fixed");
+              switchBannerElement.classList.add(...bannerClasses);
+              switchBannerElement.classList.remove(
+                "ltr:left-16",
+                "rtl:right-16",
+                "top-10"
+              );
+            }
           }
-        }
+        });
       });
-    });
 
-    observer.observe(headerElement);
-  },
+      observer.observe(headerElement);
+    },
+  };
 };
