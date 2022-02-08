d62157e3a64214a4b846f13c82ce0da4187a73ce
Timothy Koech <timosville@gmail.com>
2022-02-08 11:26:12 +0300

    feat: return user to correct locale page on 404

--- src/assets/js/errorPageViewModel.js
+++ src/assets/js/errorPageViewModel.js
@@ -1,10 +1,19 @@
 export default () => {
   return {
+    currentUserLocale: "",
     init() {
-      console.log("init 404ViewModel");
+      this.currentUserLocale = this.getUserLocale();
     },
     getUserLocale() {
-      return "en";
+      const browserLocale = navigator.language;
+      const userLocale = browserLocale.split("-")[0];
+      return userLocale;
+    },
+    goHome() {
+      const hostname = location.host;
+      const protocol = location.protocol;
+      const url = `${protocol}//${hostname}/${this.currentUserLocale}`;
+      window.location.assign(url);
     },
   };
 };
