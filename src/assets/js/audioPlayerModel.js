9ad5689abba8d13dbaa055edb5bcd3348fa2d567
Timothy Koech <timosville@gmail.com>
2022-03-03 15:46:04 +0300

    feat: add progress and total time

--- src/assets/js/audioPlayerModel.js
+++ src/assets/js/audioPlayerModel.js
@@ -1,9 +1,36 @@
 export default () => {
   return {
     isPlaying: false,
+    duration: 0,
+    seconds: 0,
+    minutes: 0,
+    hours: 0,
+    progressTime: "00:00",
+    totalTime: "00:00",
 
     init() {
-      console.log(this.$refs.audio);
+      this.$refs.audio.onloadedmetadata = () =>
+        (this.totalTime = this.formatTime(this.$refs.audio.duration));
+
+      setInterval(() => {
+        this.progressTime = this.formatTime(this.$refs.audio.currentTime);
+      });
+    },
+    formatTime(time) {
+      let hours = parseInt(time / 3600);
+      time = time % 3600;
+      let minutes = parseInt(time / 60);
+      time = time % 60;
+      let seconds = parseInt(time);
+      if (hours === 0) {
+        return `${this.padding(minutes)}:${this.padding(seconds)}`;
+      }
+      return `${this.padding(hours)}:${this.padding(minutes)}:${this.padding(
+        seconds
+      )}`;
+    },
+    padding(num) {
+      return num < 10 ? "0" + num : num;
     },
     player() {
       this.$refs.audio.paused
@@ -11,14 +38,15 @@ export default () => {
         : this.$refs.audio.pause();
       this.isPlaying = !this.isPlaying;
     },
-    speedBack() {},
-    skipBack() {},
+    speedBack() {
+      this.$refs.audio.currentTime -= 15;
+    },
     speedForwards() {
-      console.log(this.$refs.audio.paused);
-      // this.$refs.audio.playbackRate = 2;
+      this.$refs.audio.currentTime += 15;
     },
     skipForwards() {
       this.$refs.audio.pause();
     },
+    skipBack() {},
   };
 };
