942c8143368e5f2aefc74faccf7857aa0fc94fb1
Timothy Koech <timosville@gmail.com>
2022-10-27 15:48:19 +0300

    refactor: add date parser

--- src/assets/js/promo.js
+++ src/assets/js/promo.js
@@ -1,32 +1,55 @@
-import dayjs from "dayjs"
+import dayjs from 'dayjs';
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(customParseFormat);
 
 export default () => {
-    return {
-        startDate : "October 28, 2022 10:45 AM",
-        endDate : "",
-        now: dayjs(),
-        promoIsActive : false,
+  return {
+    startDate: '',
+    endDate: '',
+    now: dayjs(),
+    isPromoActive: false,
+    isValidDates: false,
 
-        init(){
-            this.showPromo()
-            console.log(this.now);
-        },
+    async init(startDate, endDate) {
+      await this.setPromoDates(startDate, endDate);
 
-        showPromo(){
-            this.$refs.promo.classList.add("flex")
-            this.$refs.promo.classList.remove("hidden")
-        },
-        hidePromo(){
-            this.$refs.promo.classList.add("hidden")
-            this.$refs.promo.classList.remove("flex")
-        }
-    }
-}
+      localStorage.getItem('promoActive') === 'true'
+        ? (this.isPromoActive = true)
+        : (this.isPromoActive = false);
 
-// localStorage.setItem("cookieConsent", "accepted");
-//       this.isCookieBoxOpen = false;
-//       console.log("box is open: ", this.isCookieBoxOpen);
-
-//       const stored = localStorage.getItem("cookieConsent");
-//       if (stored !== "accepted") return;
-//       this.isCookieBoxOpen = false;
+      this.isValidDates &&
+      dayjs().isSameOrBefore(this.endDate, 'day') &&
+      dayjs().isSameOrAfter(this.startDate, 'day') &&
+      !this.isPromoActive
+        ? this.showPromo()
+        : this.hidePromo();
+    },
+    showPromo() {
+      this.$refs.promo.classList.add('flex');
+      this.$refs.promo.classList.remove('hidden');
+    },
+    hidePromo() {
+      localStorage.setItem('promoActive', 'true');
+      this.$refs.promo.classList.add('hidden');
+      this.$refs.promo.classList.remove('flex');
+    },
+    async setPromoDates(start, end) {
+      if (
+        dayjs(start, 'YYYY-MM-DD', true).isValid() &&
+        dayjs(end, 'YYYY-MM-DD', true).isValid()
+      ) {
+        this.startDate = dayjs(start, 'YYYY-MM-DD');
+        this.endDate = dayjs(end, 'YYYY-MM-DD');
+      }
+    },
+    donate(donationUrl) {
+      localStorage.setItem('promoActive', 'true');
+      location.href = donationUrl;
+    },
+  };
+};
