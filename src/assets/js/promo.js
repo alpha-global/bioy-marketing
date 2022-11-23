5e680191d0e955e7f6983247f8e7cb068cd92fce
Timothy Koech <timosville@gmail.com>
2022-11-23 11:27:53 +0300

    Unique promo (#76)
    * feat: promo id field
    
    * feat: add promo id
    
    * refactor: remove redundant code

--- src/assets/js/promo.js
+++ src/assets/js/promo.js
@@ -13,11 +13,14 @@ export default () => {
     endDate: '',
     now: dayjs(),
     hasSeenPromo: false,
+    promoID: '',
 
-    async init(startDate, endDate) {
+    async init(startDate, endDate, promoID) {
+      this.promoID = promoID;
+
       await this.setPromoDates(startDate, endDate);
 
-      localStorage.getItem('hasSeenPromo') === 'true'
+      JSON.parse(localStorage.getItem('promoIDs'))?.includes(this.promoID)
         ? (this.hasSeenPromo = true)
         : (this.hasSeenPromo = false);
 
@@ -26,12 +29,18 @@ export default () => {
         !this.hasSeenPromo &&
         this.showPromo();
     },
+    updatePromoIDsStore(promoID) {
+      const promoIDs = JSON.parse(localStorage.getItem('promoIDs')) || [];
+      if (promoIDs?.includes(promoID)) return;
+      promoIDs.push(promoID);
+      localStorage.setItem('promoIDs', JSON.stringify(promoIDs));
+    },
     showPromo() {
       this.$refs.promoModal.classList.add('flex');
       this.$refs.promoModal.classList.remove('hidden');
     },
     hidePromo() {
-      localStorage.setItem('hasSeenPromo', 'true');
+      this.updatePromoIDsStore(this.promoID);
       this.$refs.promoModal.classList.add('hidden');
       this.$refs.promoModal.classList.remove('flex');
     },
@@ -45,7 +54,7 @@ export default () => {
       }
     },
     donate(donationUrl) {
-      localStorage.setItem('hasSeenPromo', 'true');
+      this.updatePromoIDsStore(this.promoID);
       location.href = donationUrl;
     },
   };
