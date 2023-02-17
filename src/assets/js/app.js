8a0f4013f1a95156994001999394fbe59d9189cd
Timothy Koech <timosville@gmail.com>
2023-02-17 11:15:18 +0300

    chore: remove dead wood

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,23 +1,20 @@
-import Alpine from "alpinejs";
+import Alpine from 'alpinejs';
 
-import audioPlayer from "./audioPlayerModel.js";
-import banner from "./bannerModel.js";
-import baseView from "./baseViewModel.js";
-import devotionBanner from "./devotionBannerModel.js";
-import errorPageView from "./errorPageViewModel.js";
-import faqsView from "./faqsViewModel.js";
-import mediumView from "./mediumViewModel.js";
-import promo from "./promo.js";
-import testimonialsSlider from "./testimonialsSliderModel.js";
+import audioPlayer from './audioPlayerModel.js';
+import baseView from './baseViewModel.js';
+import devotionBanner from './devotionBannerModel.js';
+import errorPageView from './errorPageViewModel.js';
+import faqsView from './faqsViewModel.js';
+import mediumView from './mediumViewModel.js';
+import promo from './promo.js';
+import testimonialsSlider from './testimonialsSliderModel.js';
 
-
-Alpine.data("audioPlayer", audioPlayer);
-Alpine.data("banner", banner);
-Alpine.data("baseView", baseView);
-Alpine.data("devotionBanner", devotionBanner);
-Alpine.data("faqsView", faqsView);
-Alpine.data("errorPageView", errorPageView);
-Alpine.data("mediumView", mediumView);
-Alpine.data("promo", promo);
-Alpine.data("testimonialsSlider", testimonialsSlider);
+Alpine.data('audioPlayer', audioPlayer);
+Alpine.data('baseView', baseView);
+Alpine.data('devotionBanner', devotionBanner);
+Alpine.data('faqsView', faqsView);
+Alpine.data('errorPageView', errorPageView);
+Alpine.data('mediumView', mediumView);
+Alpine.data('promo', promo);
+Alpine.data('testimonialsSlider', testimonialsSlider);
 Alpine.start();
