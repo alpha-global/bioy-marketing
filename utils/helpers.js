09c78b930f99057b7b192b4234f60c35286359d7
Timothy Koech <timosville@gmail.com>
2023-11-10 10:15:04 +0300

    wip

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,13 +1,23 @@
 const marked = require('marked');
 
+const walkTokens = (token) => {
+  if (token.type === 'heading') {
+    token.depth += 2;
+  }
+};
+
 const toHtml = (markdown) => {
   if (!markdown) return '';
-  let rendered = marked
-    .marked(markdown)
-    .replaceAll(/<code>/gm, '<sup>')
-    .replaceAll(/<\/code>/gm, '</sup>')
-    .replaceAll('|', '&nbsp;');
-  return rendered;
+  let tokens = marked.lexer(markdown);
+  tokens.forEach(walkTokens);
+  return marked.parser(tokens);
+
+  // let rendered = marked
+  //   .marked(markdown)
+  //   .replaceAll(/<code>/gm, '<sup>')
+  //   .replaceAll(/<\/code>/gm, '</sup>')
+  //   .replaceAll('|', '&nbsp;');
+  // return rendered;
 };
 
 module.exports = {
