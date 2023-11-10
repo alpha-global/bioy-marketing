bc6ff7293e9a862ed5f2e25a83f276a9c47e6db9
Timothy Koech <timosville@gmail.com>
2023-11-10 13:58:40 +0300

    chore: restore markdown

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,23 +1,13 @@
 const marked = require('marked');
 
-const walkTokens = (token) => {
-  if (token.type === 'heading') {
-    token.depth += 2;
-  }
-};
-
 const toHtml = (markdown) => {
   if (!markdown) return '';
-  let tokens = marked.lexer(markdown);
-  tokens.forEach(walkTokens);
-  return marked.parser(tokens);
-
-  // let rendered = marked
-  //   .marked(markdown)
-  //   .replaceAll(/<code>/gm, '<sup>')
-  //   .replaceAll(/<\/code>/gm, '</sup>')
-  //   .replaceAll('|', '&nbsp;');
-  // return rendered;
+  let rendered = marked
+    .marked(markdown)
+    .replaceAll(/<code>/gm, '<sup>')
+    .replaceAll(/<\/code>/gm, '</sup>')
+    .replaceAll('|', '&nbsp;');
+  return rendered;
 };
 
 module.exports = {
