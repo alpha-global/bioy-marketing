5ce3489a1759ec4d1d5eea993325b7aff5c5919c
Timothy Koech <timosville@gmail.com>
2023-12-13 08:23:40 +0300

    refactor: update classes to toggle

--- src/assets/js/devotionBannerModel.js
+++ src/assets/js/devotionBannerModel.js
@@ -14,23 +14,23 @@ export default () => {
     },
 
     switchBanner() {
-      const headerElement = document.getElementById("header");
+      const headerElement = document.getElementById('header');
 
-      const switchBannerElement = document.getElementById("switchBanner");
+      const switchBannerElement = document.getElementById('switchBanner');
 
       const bannerClasses = [
-        "top-0",
-        "inset-x-0",
-        "justify-center",
-        "bg-white",
-        "max-w-2xl",
-        "mx-auto",
-        "divide-y-1",
-        "border-b-2",
-        "border-red",
-        "divide-x-2",
-        "rtl:divide-x-reverse",
-        "divide-grey-light",
+        'top-0',
+        'inset-x-0',
+        'justify-center',
+        'bg-white',
+        'max-w-2xl',
+        'mx-auto',
+        'divide-y-1',
+        'border-b-2',
+        'border-red',
+        'divide-x-2',
+        'rtl:divide-x-reverse',
+        'divide-grey-light',
       ];
 
       const observer = new IntersectionObserver((entries) => {
@@ -38,21 +38,25 @@ export default () => {
           if (this.isDevotion) {
             if (entry.intersectionRatio > 0) {
               this.outOfView = false;
-              switchBannerElement.classList.replace("fixed", "absolute");
+              switchBannerElement.classList.replace('fixed', 'absolute');
               switchBannerElement.classList.remove(...bannerClasses);
               switchBannerElement.classList.add(
-                "ltr:left-16",
-                "rtl:right-16",
-                "top-10"
+                'top-10',
+                'ltr:left-[9rem]',
+                'sm:ltr:left-[10rem]',
+                'md:ltr:left-[11rem]',
+                'rtl:right-16',
               );
             } else {
               this.outOfView = true;
-              switchBannerElement.classList.replace("absolute", "fixed");
+              switchBannerElement.classList.replace('absolute', 'fixed');
               switchBannerElement.classList.add(...bannerClasses);
               switchBannerElement.classList.remove(
-                "ltr:left-16",
-                "rtl:right-16",
-                "top-10"
+                'top-10',
+                'ltr:left-[9rem]',
+                'sm:ltr:left-[10rem]',
+                'md:ltr:left-[11rem]',
+                'rtl:right-16',
               );
             }
           }
