fb82a2cb5776c91f026177b10d639606bf3c042f
Timothy Koech <timosville@gmail.com>
2024-11-07 13:02:14 +0300

    Feat/present marketing materials (#116)
    * chore: add branch
    
    * Update Core Pages “FAQs”
    
    * Update Core Pages “FAQs”
    
    * wip
    
    * wip
    
    * wip
    
    * Update Core Pages “promote”
    
    * Update Core Pages “promote”
    
    * chore: clear data file
    
    * Update Core Pages “promote”
    
    * refactor: improve string handling in protectRunts function
    
    * refactor: organize data files
    
    * chore: clean-up
    
    * wip: populate
    
    * wip:
    
    * chore: change data file location
    
    * chore: restructure file
    
    * Update Core Pages “promote”
    
    * Update Core Pages “promote”
    
    * Update Core Pages “promote”
    
    * refactor: clean-up data file
    
    * chore: update data file
    
    * refactor: get strings from data file
    
    * Update Core Pages “promote”
    
    * Update Repeated Content Blocks “footer”
    
    * refactor: remove debugger
    
    * feat: add share template
    
    * feat: define shape of promote data
    
    * feat: set text to read
    
    * fix: correct spelling of "Download" and update promotional pack link
    
    * chore: clean-up
    
    * fix: update backend branch to feat/present-marketing-materials
    
    * Update Core Pages “promote”
    
    * fix: update backend branch to main
    
    ---------
    
    Co-authored-by: timothy <timothy@scoutredeem.co>

--- utils/filters.js
+++ utils/filters.js
@@ -5,19 +5,17 @@ module.exports = {
   protectRunts: (str) => {
     if (!str) { return; }
     if (str.search("<br>") === -1) {
-      let title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
+      const title = str.replace(/((.*)\s(.*))$/g, "$2&nbsp;$3");
       return title.replace(/"(.*)"/g, '\\"$1\\"');
     }
-    else {
-      let phrase = str.split("<br>");
-      let first = phrase[0]
-        .trimEnd()
-        .replace(/((.*)\s(.*))$/g, "$2&nbsp;$3")
-        .replace(/"(.*)"/g, '\\"$1\\"');
-      let last = phrase[1].trim();
-      let title = first.concat(" <br>", last);
-      return title
-    }
+    const phrase = str.split("<br>");
+    const first = phrase[0]
+      .trimEnd()
+      .replace(/((.*)\s(.*))$/g, "$2&nbsp;$3")
+      .replace(/"(.*)"/g, '\\"$1\\"');
+    const last = phrase[1].trim();
+    const title = first.concat(" <br>", last);
+    return title;
   },
 
   htmlDateString: (dateObj) => {
@@ -26,7 +24,11 @@ module.exports = {
 
   devotionList: (devotions, locale, variant) => {
     return devotions.filter((devo) => {
-      return devo.locale == locale && devo.variant == variant && devo.number <= today
+      return devo.locale === locale && devo.variant === variant && devo.number <= today
     });
-  }
+  },
+
+  highlightWord: (str) => {
+    return str.replace(/^(\w+)/, '<span class="text-red">$1</span>');
+  },
 }
